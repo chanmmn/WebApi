f4c16152cad21a21e07643cf348d3d28f028125d
chanmmn <33271651+chanmmn@users.noreply.github.com>
2023-11-12 21:58:46 +0800

    Commit WebApiOrderDemo
    Commit WebApiOrderDemo

---
+++ WebApiOrderDemo/WebApiOrderDemo/Models/OrderContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiOrderDemo.Models;
+
+public partial class OrderContext : DbContext
+{
+    public OrderContext()
+    {
+    }
+
+    public OrderContext(DbContextOptions<OrderContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost;Database=order;Trusted_Connection=True;TrustServerCertificate=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasKey(e => e.OrderId).HasName("PK_Order");
+
+            entity.Property(e => e.OrderId).ValueGeneratedNever();
+            entity.Property(e => e.OrderDate).HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<OrderDetail>(entity =>
+        {
+            entity.ToTable("OrderDetail");
+
+            entity.Property(e => e.OrderDetailId).ValueGeneratedNever();
+
+            entity.HasOne(d => d.Order).WithMany(p => p.OrderDetails)
+                .HasForeignKey(d => d.OrderId)
+                .HasConstraintName("FK_OrderDetail_Order");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ WebApiOrderDemo/WebApiOrderDemo/Models/OrderDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApiOrderDemo.Models;
+
+public partial class OrderDetail
+{
+    public Guid OrderDetailId { get; set; }
+
+    public Guid? OrderId { get; set; }
+
+    public Guid? ProductId { get; set; }
+
+    public int? Quantity { get; set; }
+
+    public virtual Order? Order { get; set; }
+}
